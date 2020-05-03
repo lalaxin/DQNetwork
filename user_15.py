0b7e3941c959657f946990ffdfa3e5c182f2eacf
unknown <2252328984@qq.com>
2020-05-03 15:00:03 +0800

    (1234)

--- user_15.py
+++ user_15.py
@@ -2,52 +2,32 @@ import xlrd,xlwt
 import datetime
 import random
 
-def getusers():
-    # 打开需要操作的excel,由于地图左下角区域没有用户，所以不处理那些区域（原点定为（8000，7000））
-    excel = xlrd.open_workbook("./data2.xlsx")
+class getuser_15():
+    def getusers(self):
+        # 打开需要操作的excel,由于地图左下角区域没有用户，所以不处理那些区域（原点定为（8000，7000））
+        excel = xlrd.open_workbook("./data2.xlsx")
 
-    # 获取excel的sheet表
-    sheet = excel.sheet_by_name("Sheet1")
-    starttime = datetime.datetime(2016, 8, 1, 0, 00, 00)
-    endtime = datetime.datetime(2016, 8, 1, 1, 00, 00)
+        # 获取excel的sheet表
+        sheet = excel.sheet_by_name("Sheet1")
+        starttime = datetime.datetime(2016, 8, 1, 0, 00, 00)
+        endtime = datetime.datetime(2016, 8, 1, 1, 00, 00)
 
-    #统计截至日期
-    deadday = datetime.datetime(2016,8,16,0,00,00)
+        #统计截至日期
+        deadday = datetime.datetime(2016,8,16,0,00,00)
 
-    onetime = []
-    one = []
-    user = []
-    print("-------start----")
-    print("start", starttime)
-    print("end", endtime)
-
-    for i in range(1, sheet.nrows):
-        row_i = sheet.row_values(i)
-        d = xlrd.xldate_as_datetime(row_i[0], 0)
-        # print(i,": ",row_i)
         onetime = []
-        if d > starttime and d <= endtime:
-            onetime.append(row_i[2])
-            onetime.append(row_i[4])
-            onetime.append(row_i[7])
-            onetime.append(row_i[9])
-            onetime.append(random.uniform(0, 200))
-            onetime.append(-1)
-            onetime.append(-1)
-            one.append(onetime)
-            # print("d",d)
-            # print("onetime:",onetime)
-        else:
-            # print("one:", one)
-            # print(len(one))
-            user.append(one)
-            # print("user:", user)
-            one = []
-            if d >= endtime:
-                starttime = endtime
-                print("start", starttime)
-                endtime = endtime + datetime.timedelta(hours=1)
-                print("end", endtime)
+        one = []
+        user = []
+        print("-------start----")
+        print("start", starttime)
+        print("end", endtime)
+
+        for i in range(1, sheet.nrows):
+            row_i = sheet.row_values(i)
+            d = xlrd.xldate_as_datetime(row_i[0], 0)
+            # print(i,": ",row_i)
+            onetime = []
+            if d > starttime and d <= endtime:
                 onetime.append(row_i[2])
                 onetime.append(row_i[4])
                 onetime.append(row_i[7])
@@ -55,16 +35,75 @@ def getusers():
                 onetime.append(random.uniform(0, 200))
                 onetime.append(-1)
                 onetime.append(-1)
-                # print("onetime:", onetime)
                 one.append(onetime)
-                # print(len(one))
+                # print("d",d)
+                # print("onetime:",onetime)
+            else:
                 # print("one:", one)
-        if starttime >= deadday:
-            d = xlrd.xldate_as_datetime(row_i[0], 0)
-            print("d:",d)
-            break
-    # print(user)
-    return user
+                # print(len(one))
+                user.append(one)
+                # print("user:", user)
+                one = []
+                if d >= endtime:
+                    starttime = endtime
+                    print("start", starttime)
+                    endtime = endtime + datetime.timedelta(hours=1)
+                    print("end", endtime)
+                    onetime.append(row_i[2])
+                    onetime.append(row_i[4])
+                    onetime.append(row_i[7])
+                    onetime.append(row_i[9])
+                    onetime.append(random.uniform(0, 200))
+                    onetime.append(-1)
+                    onetime.append(-1)
+                    # print("onetime:", onetime)
+                    one.append(onetime)
+                    # print(len(one))
+                    # print("one:", one)
+            if starttime >= deadday:
+                d = xlrd.xldate_as_datetime(row_i[0], 0)
+                print("d:",d)
+                break
+        # print(user)
+        return user
 
-if __name__  == '__main__':
-    u = getusers()
+    def usertoregion(self,user,region,cell,celllength):
+        b=0
+        for i in range(len(user)):
+            if (user[i][0] == cell * celllength and user[i][1] == cell * celllength):
+                a = int(cell*cell - 1)
+            elif (user[i][0] == cell * celllength):
+                a = int(user[i][1] / celllength) * cell + int(user[i][0] / celllength) - 1
+            elif (user[i][1] == cell * celllength):
+                a = int(user[i][1] / celllength) * cell + int(user[i][0] / celllength) - cell
+            else:
+                a = int(user[i][1] / celllength) * cell + int(user[i][0] / celllength)
+            # print(a)
+            if (a < cell*cell):
+                region[a] += 1
+
+            if(b<a):
+                b=a
+        print(b)
+        return region
+
+def getregion():
+    users=getuser_15().getusers()
+    print(len(users))
+    cell=10
+    celllength=300
+    T=23*15
+    init_region=[[0 for i in range (cell*cell)]for t in range(T)]
+    # print("initregion",init_region)
+    for t in range(T):
+        print("user[t]",len(users[t]),users[t])
+        init_region[t]=getuser_15().usertoregion(users[t],init_region[t],cell,celllength)
+        print("init_region[t]",t,init_region[t])
+
+        # for i in range(cell*cell):
+        #     init_region[i]=0
+
+
+getregion()
+# if __name__  == '__main__':
+#     u = getusers()
